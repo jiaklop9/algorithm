5badfe1a8891c021261a40e061c9330f72a3a747
lzk <18600795349@163.com>
2020-11-21 22:42:03 +0800

    栈

---
+++ stack.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+class Stack(object):
+    def __init__(self):
+        self.items = list()
+
+    def push(self, data):
+        self.items.append(data)
+
+    def pop(self):
+        self.items.pop()
+
+    def peek(self):
+        """
+        返回栈顶项但不删除
+        :return:
+        """
+        return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
